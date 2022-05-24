334069b7327fde09009eb1b28eeee6dc7ce7e9e7
Anton Wieslander <Anton.Wieslander@crispthinking.com>
2022-05-24 15:46:16 +0100

    struct ctor

---
+++ Playground/UseCases/012_struct_ctor.cs
@@ -0,0 +1,18 @@
+﻿using Shpec;
+namespace Playground.UseCases;
+
+
+public partial class struct_ctor : IUseCase
+{
+    Properties _p => new(
+        Property.FirstName,
+        Property.LastName,
+        Property.Age
+    );
+    
+    public void Execute()
+    {
+        var r = new struct_ctor("Foo", "Bar", 5);
+        Console.WriteLine(r);
+    }
+}
