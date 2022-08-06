8b7e7ed722d3734000824801f3590e4efc6ff015
Anton Wieslander <Anton.Wieslander@crispthinking.com>
2022-08-06 20:41:27 +0100

    simple build and run playground tests

--- build/Build.cs
+++ build/Build.cs
@@ -1,44 +1,59 @@
-using System;
-using System.Linq;
 using Nuke.Common;
-using Nuke.Common.CI;
-using Nuke.Common.Execution;
 using Nuke.Common.IO;
-using Nuke.Common.ProjectModel;
-using Nuke.Common.Tooling;
+using Nuke.Common.Tools.DotNet;
 using Nuke.Common.Utilities.Collections;
-using static Nuke.Common.EnvironmentInfo;
+using static Nuke.Common.Tools.DotNet.DotNetTasks;
 using static Nuke.Common.IO.FileSystemTasks;
-using static Nuke.Common.IO.PathConstruction;
 
 class Build : NukeBuild
 {
-    /// Support plugins are available for:
-    ///   - JetBrains ReSharper        https://nuke.build/resharper
-    ///   - JetBrains Rider            https://nuke.build/rider
-    ///   - Microsoft VisualStudio     https://nuke.build/visualstudio
-    ///   - Microsoft VSCode           https://nuke.build/vscode
+    public static int Main() => Execute<Build>(x => x.Run);
 
-    public static int Main () => Execute<Build>(x => x.Compile);
+    [Parameter("Configuration to build - Default is 'Debug' (local) or 'Release' (server)")] readonly Configuration Configuration = IsLocalBuild ? Configuration.Debug : Configuration.Release;
 
-    [Parameter("Configuration to build - Default is 'Debug' (local) or 'Release' (server)")]
-    readonly Configuration Configuration = IsLocalBuild ? Configuration.Debug : Configuration.Release;
+    AbsolutePath OutputDirectory => RootDirectory / "output";
+    AbsolutePath SourceDirectory => RootDirectory / "src";
+    AbsolutePath TestDirectory => RootDirectory / "test";
+    AbsolutePath PlaygroundDirectory => TestDirectory / "Playground";
 
     Target Clean => _ => _
-        .Before(Restore)
         .Executes(() =>
         {
+            SourceDirectory.GlobDirectories("*/bin", "*/obj").ForEach(DeleteDirectory);
+            TestDirectory.GlobDirectories("*/bin", "*/obj").ForEach(DeleteDirectory);
+            EnsureCleanDirectory(OutputDirectory);
         });
 
     Target Restore => _ => _
+        .DependsOn(Clean)
         .Executes(() =>
         {
+            DotNetRestore(_ => _.SetProjectFile(RootDirectory));
         });
 
     Target Compile => _ => _
         .DependsOn(Restore)
         .Executes(() =>
         {
+            DotNetBuild(_ => _
+                .SetProjectFile(RootDirectory)
+                .SetConfiguration(Configuration)
+            );
         });
 
+    Target TestPlayground => _ => _
+        .DependsOn(Compile)
+        .Executes(() =>
+        {
+            DotNetRun(_ => _
+                .SetProjectFile(PlaygroundDirectory)
+                .SetConfiguration(Configuration)
+            );
+        });
+
+    Target Run => _ => _
+        .DependsOn(TestPlayground)
+        .Executes(() =>
+        {
+        });
 }
